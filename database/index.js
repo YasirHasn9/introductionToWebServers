266c61827d2e1711627eeabdaa41014a6d49266f
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-10-29 23:08:18 -0700

    create schema

--- database/index.js
+++ database/index.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
-
-const usersSchema  = mongoose.Schema({
-    name:String,
-    bio: String
-})
-
-
-module.exports = mongoose.model("Users" , usersSchema)
+const productSchema = new mongoose.Schema({
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true }
+});
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
