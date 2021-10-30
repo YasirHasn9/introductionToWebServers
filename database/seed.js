aca4b07dcf360935f919ce4080d328ddd12aaca8
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-10-29 23:09:35 -0700

    create seeds

---
+++ database/seed.js
@@ -0,0 +1,45 @@
+const Product = require("./index");
+const mongoose = require("mongoose");
+//create your array. i inserted only 1 object here
+const products = [   
+  new Product({
+    image:
+      "https://static.seattletimes.com/wp-content/uploads/2018/01/a8e801dc-f665-11e7-bf8f-ddd02ba4a187-780x1181.jpg",
+    title: "Origin",
+    author: "Dan Brown",
+    description:
+      "2017 mystery thriller novel. Dan Brown is back with another thriller so moronic you can feel your IQ points flaking away like dandruff",
+    price: 12
+  }),
+  new Product({
+    image:
+      "https://static.seattletimes.com/wp-content/uploads/2018/01/a8e801dc-f665-11e7-bf8f-ddd02ba4a187-780x1181.jpg",
+    title: "Origin",
+    author: "Dan Brown",
+    description:
+      "2017 mystery thriller novel. Dan Brown is back with another thriller so moronic you can feel your IQ points flaking away like dandruff",
+    price: 12
+  })
+]
+
+const uri = process.env.URI || undefined
+//connect mongoose
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .catch(err => {
+    console.log(err.stack);
+    process.exit(1);
+  })
+  .then(() => {
+    console.log("connected to db in development environment");
+  });
+//save your data. this is an async operation
+//after you make sure you seeded all the products, disconnect automatically
+products.map(async (p, index) => {
+  await p.save((err, result) => {
+    if (index === products.length - 1) {
+      console.log("DONE!");
+      mongoose.disconnect();
+    }
+  });
+});
