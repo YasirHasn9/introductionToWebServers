8a96195e5b4281de0e0b1e8d878e8e6996b20fe1
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-10-29 23:11:27 -0700

    connect server

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-// // require the http module
-// const http = require("http")
-// // specify the hostname and the port that we want our server to run on
-// const hostName = "127.0.0.1"
-// const port = 3000
-
-
-// // create a server form the http
-// const server = http.createServer((req, res) => {
-//     // req --> Request --> request provide the requested details
-//     // res --> Response --> response provides the answer for the request
-//     // These object handle the http call.
-//     res.statusCode = 200 // indicates a successful request
-//     res.setHeader("Context-type", "text/plain")
-//     res.end("hello world") // end/close the response
-// })
-
-// // listen to the server on the specified prot and hostname, when the server is ready the cb will be executed
-// server.listen(port , hostName , () => {
-//     console.log(`Server running at http://${hostName}:${port}/`)
-// })
-
-
+require("dotenv").config()
 const express = require("express")
-const server = express()
-const port = 5000
-const usersRouter = require("./routers/index")
+const mongoose = require("mongoose")
+const app = express()
+const port = process.env.PORT || 3000
+const uri = process.env.URI || undefined
+//routers 
+const productRouter = require("./routers/index")
 
+mongoose.connect(uri)
+app.use("/products" , productRouter)
+app.use("/" , (req, res) => {
+    res.send({
+        msg: "Hello world"
+    })
+})
 
-server.use("/users" , usersRouter)
-server.listen(port , () => console.log(`http://localhost:${port}`))
+app.listen(port , () => console.log(`http://localhost:${port}`))
