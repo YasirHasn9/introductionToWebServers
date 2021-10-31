bcbcbd19612f72a1751a471fce2eb452565b799d
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-10-31 13:06:46 -0700

    create router

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,11 @@
-const { Router } = require("express")
 const express = require("express")
 const router = express.Router()
 const db = require("../database/index")
 
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+  })
 router.get("/" , async (req,res) => {
     try{
         const products = await db.find()
